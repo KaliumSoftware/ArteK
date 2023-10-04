ec07d86bf9ad40af461d72d00a362bf356e6dbe5
franjunoy <franjunoy@hotmail.com>
2023-10-03 22:39:07 -0300

    paintbrush

---
+++ server/models/paintbrush.js
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose';
+
+const paintbrushSchema = new Schema({
+  price: { type: Number },
+  color: { type: String },
+  model: { type: String },
+  modelLower: { type: String },
+  description: { type: String },
+  image: { publicId: String, url: String },
+  reviews: { type: Array },
+  averageRating: { type: Number },
+  active: { type: Boolean }
+});
+
+paintbrushSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+const Paintbrush = model('Paintbrush', paintbrushSchema);
+
+export default Paintbrush;
