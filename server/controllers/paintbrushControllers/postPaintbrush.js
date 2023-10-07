da0b56ae7ac8736304d8a6ffd13f0b77fa2e1f94
franjunoy <franjunoy@hotmail.com>
2023-10-07 00:50:44 -0300

    postPaintbrush

---
+++ server/controllers/paintbrushControllers/postPaintbrush.js
@@ -0,0 +1,41 @@
+import { Paint } from '../../models/paint';
+
+const postPaint = async (price, model, color, amount, description, image) => {
+  try {
+    const modelLower = model.toLowerCase();
+    const colorLower = color.toLowerCase();
+    const amountLower = amount.toLowerCase();
+
+    let existingPaint = await Paint.findOne({
+      modelLower,
+      colorLower,
+      amountLower
+    });
+
+    if (existingPaint) {
+      if (existingPaint.isActive) {
+        throw new Error('Pintura repetida');
+      } else {
+        existingPaint.isActive = true;
+        await existingService.save();
+        return 'La pintura fue agregada';
+      }
+    } else {
+      await Service.create({
+        price,
+        modelLower,
+        colorLower,
+        amountLower,
+        description,
+        image
+      });
+      return {
+        message: 'La pintura fue agregada'
+      };
+    }
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = postPaint;
