e94e69d6b40fdfe01282f8aaa21b7bbfd7d2a636
franjunoy <franjunoy@hotmail.com>
2023-10-13 01:10:38 -0300

    deleteUser

---
+++ server/controllers/userControllers/deleteUser.js
@@ -0,0 +1,10 @@
+import { User } from '../../models/user';
+
+const deleteUser = async (newUser) => {
+  try {
+  } catch (error) {
+    return error.message;
+  }
+};
+
+module.exports = deleteUser;
