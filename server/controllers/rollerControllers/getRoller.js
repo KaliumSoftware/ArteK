c178d322078a74ced94e0c2c03d6f7de019c5bff
franjunoy <franjunoy@hotmail.com>
2023-10-13 00:53:10 -0300

    getRoller

--- server/controllers/rollerControllers/getRoller.js
+++ server/controllers/rollerControllers/getRoller.js
@@ -1,8 +1,8 @@
-import { Paint } from '../../models/paint';
+import { Roller } from '../../models/roller';
 
 const getRoller = async () => {
   try {
-    const rollers = await Paint.find({ isActive: true });
+    const rollers = await Roller.find({ isActive: true });
     if (rollers.length === 0) {
       throw new Error('No hay rodillos');
     } else {
