b5b4bf0ee79889fb55370d580a55f03f3ce094c5
franjunoy <franjunoy@hotmail.com>
2023-10-17 15:36:27 -0300

    postUser

--- server/controllers/userControllers/postUser.js
+++ server/controllers/userControllers/postUser.js
@@ -2,20 +2,19 @@ import { User } from '../../models/user';
 
 const postUser = async (newUser) => {
   try {
-    const usernameRepeated = await User.findOne({
-      usernameLower: newUser.usernameLower
-    });
     const emailRepeated = await User.findOne({
       email: newUser.email
     });
-    if (emailRepeated) throw new Error('emailRepetido');
-    if (usernameRepeated) throw new Error('usernameRepetido');
-
-    await User.create(newUser);
-    return true;
+    if (emailRepeated) {
+      throw new Error('Ya existe ese email');
+    } else {
+      await User.create(newUser);
+    }
+    return {
+      message: 'Usuario creado'
+    };
   } catch (error) {
     return error.message;
   }
 };
-
 module.exports = postUser;
