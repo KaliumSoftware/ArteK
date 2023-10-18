6ba3cd3e5451d0906769d3ca48138f631ed4d5e4
franjunoy <franjunoy@hotmail.com>
2023-10-18 19:18:42 -0300

    deleteUser

--- server/controllers/userControllers/deleteUser.js
+++ server/controllers/userControllers/deleteUser.js
@@ -1,9 +1,20 @@
 import { User } from '../../models/user';
 
-const deleteUser = async (newUser) => {
+const deleteUser = async (id) => {
   try {
+    const user = await User.findOne({ _id: id });
+
+    if (!user || user.isActive === false) {
+      throw new Error('No se encontró usuario');
+    } else {
+      user.isActive = false;
+      await consumerUser.save();
+      return {
+        message: 'Usuario eliminado'
+      };
+    }
   } catch (error) {
-    return error.message;
+    return false;
   }
 };
 
