c0f16a364b23c11953e4254ea27920437a519610
franjunoy <franjunoy@hotmail.com>
2023-10-19 21:27:52 -0300

    getUser

--- server/controllers/userControllers/getUser.js
+++ server/controllers/userControllers/getUser.js
@@ -2,8 +2,16 @@ import { User } from '../../models/user';
 
 const getUser = async (newUser) => {
   try {
+    const consumer = await Consumer.findOne(
+      { userUid: userUid },
+      {
+        password: 0,
+        usernameLower: 0
+      }
+    );
+    return consumer ? consumer : false;
   } catch (error) {
-    return error.message;
+    return false;
   }
 };
 
