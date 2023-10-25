89b0a1f5693369948a42232e47166f13379eb0b2
franjunoy <franjunoy@hotmail.com>
2023-10-25 00:39:31 -0300

    Handlers

---
+++ server/handlers/userHandlers/putUser.js
@@ -0,0 +1,21 @@
+import { User } from '../../models/user';
+
+const putUser = async (id) => {
+  try {
+    const user = await User.findOne({ _id: id });
+
+    if (user || user.isActive === true) {
+      throw new Error('El usuario ya existe');
+    } else {
+      user.isActive = true;
+      await consumerUser.save();
+      return {
+        message: 'Usuario restablecido'
+      };
+    }
+  } catch (error) {
+    return error.message;
+  }
+};
+
+module.exports = putUser;
