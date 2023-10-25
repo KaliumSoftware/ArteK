c962cc72ce206e20ca1f54a6aeb508d670fb3ae3
franjunoy <franjunoy@hotmail.com>
2023-10-25 00:38:58 -0300

    Handlers

---
+++ server/handlers/rollerHandlers/getRoller.js
@@ -0,0 +1,16 @@
+import { Roller } from '../../models/roller';
+
+const getRoller = async () => {
+  try {
+    const rollers = await Roller.find({ isActive: true });
+    if (rollers.length === 0) {
+      throw new Error('No hay rodillos');
+    } else {
+      return rollers;
+    }
+  } catch (error) {
+    return error.message;
+  }
+};
+
+module.exports = getRoller;
