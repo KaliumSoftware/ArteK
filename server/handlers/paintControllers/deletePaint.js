22d2e1420cb174c763bf23f0e0d4ab3be14c0d79
franjunoy <franjunoy@hotmail.com>
2023-10-25 00:34:02 -0300

    Handlers

---
+++ server/handlers/paintControllers/deletePaint.js
@@ -0,0 +1,21 @@
+import { Paint } from '../../models/paint';
+
+const deletePaint = async (id) => {
+  try {
+    const existingPaint = await Paint.findOne({ _id: id });
+
+    if (!existingPaint || existingPaint.isActive === false) {
+      throw new Error('No se encontró la pintura');
+    } else {
+      existingPaint.isActive = false;
+      await existingPaint.save();
+      return {
+        message: 'Eliminaste la pintura'
+      };
+    }
+  } catch (error) {
+    return error.message;
+  }
+};
+
+module.exports = deletePaint;
