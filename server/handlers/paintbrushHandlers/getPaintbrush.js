66ab9f71fbca4dab1c93db46f4ded3b2079a010e
franjunoy <franjunoy@hotmail.com>
2023-10-25 00:37:17 -0300

    Handlers

---
+++ server/handlers/paintbrushHandlers/getPaintbrush.js
@@ -0,0 +1,16 @@
+import { Paintbrush } from '../../models/paintbrush';
+
+const getPaintbrush = async () => {
+  try {
+    const paintbrush = await Paintbrush.find({ isActive: true });
+    if (paintbrush.length === 0) {
+      throw new Error('No hay cepillos');
+    } else {
+      return paintbrush;
+    }
+  } catch (error) {
+    return error.message;
+  }
+};
+
+module.exports = getPaintbrush;
