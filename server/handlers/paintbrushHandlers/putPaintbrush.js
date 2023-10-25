f6a9e6a714587b1983bf87a23bba30bd85d5a07a
franjunoy <franjunoy@hotmail.com>
2023-10-25 00:37:24 -0300

    Handlers

---
+++ server/handlers/paintbrushHandlers/putPaintbrush.js
@@ -0,0 +1,49 @@
+import { Paintbrush } from '../../models/paintbrush';
+
+const putPaintbrush = async (
+  id,
+  price,
+  model,
+  color,
+  size,
+  description,
+  image
+) => {
+  try {
+    const modelLower = model.toLowerCase();
+    const colorLower = color.toLowerCase();
+    const sizeLower = size.toLowerCase();
+    const existingPaintbrush = await Paintbrush.findOne({ _id: id });
+
+    if (!existingPaintbrush || existingPaintbrush.isActive === false) {
+      throw new Error('El cepillo no existe');
+    } else {
+      if (price) {
+        existingPaintbrush.price = price;
+      }
+      if (model) {
+        existingPaintbrush.model = modelLower;
+      }
+      if (color) {
+        existingPaintbrush.color = colorLower;
+      }
+      if (size) {
+        existingPaintbrush.size = sizeLower;
+      }
+      if (description) {
+        existingPaintbrush.description = description;
+      }
+      if (image) {
+        existingPaintbrush.image = image;
+      }
+      await existingPaintbrush.save();
+      return {
+        message: 'El cepillo fue actualizado'
+      };
+    }
+  } catch (error) {
+    return error.message;
+  }
+};
+
+module.exports = putPaintbrush;
