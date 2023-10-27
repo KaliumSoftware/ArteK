cd786dc7b972799861469bbdf882f8127b7add6d
franjunoy <franjunoy@hotmail.com>
2023-10-27 18:12:04 -0300

    Handler

--- server/handlers/userHandlers/handlerDeleteUser.js
+++ server/handlers/userHandlers/handlerDeleteUser.js
@@ -2,17 +2,6 @@ import { User } from '../../models/user';
 
 const deleteUser = async (id) => {
   try {
-    const user = await User.findOne({ _id: id });
-
-    if (!user || user.isActive === false) {
-      throw new Error('No se encontró usuario');
-    } else {
-      user.isActive = false;
-      await consumerUser.save();
-      return {
-        message: 'Usuario eliminado'
-      };
-    }
   } catch (error) {
     return error.message;
   }
