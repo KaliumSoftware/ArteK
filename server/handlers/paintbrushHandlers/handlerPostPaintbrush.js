faca304a281703293712cebb5e4ef06cd8191f7d
franjunoy <franjunoy@hotmail.com>
2023-10-27 18:10:58 -0300

    Handler

--- server/handlers/paintbrushHandlers/handlerPostPaintbrush.js
+++ server/handlers/paintbrushHandlers/handlerPostPaintbrush.js
@@ -9,37 +9,6 @@ const postPaintbrush = async (
   image
 ) => {
   try {
-    const modelLower = model.toLowerCase();
-    const colorLower = color.toLowerCase();
-    const sizeLower = size.toLowerCase();
-
-    let existingPaintbrush = await Paintbrush.findOne({
-      modelLower,
-      colorLower,
-      sizeLower
-    });
-
-    if (existingPaintbrush) {
-      if (existingPaintbrush.isActive) {
-        throw new Error('El cepillo ya existe');
-      } else {
-        existingPaintbrush.isActive = true;
-        await existingPaintbrush.save();
-        return 'El cepillo fue agregado';
-      }
-    } else {
-      await Paintbrush.create({
-        price,
-        model,
-        color,
-        size,
-        description,
-        image
-      });
-      return {
-        message: 'El cepillo fue agregado'
-      };
-    }
   } catch (error) {
     return error.message;
   }
