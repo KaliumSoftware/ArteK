af244ab78491822d918e921b68293661b7ec9422
franjunoy <franjunoy@hotmail.com>
2023-10-27 18:11:50 -0300

    Handler

--- server/handlers/paintHandlers/handlerPutPaint.js
+++ server/handlers/paintHandlers/handlerPutPaint.js
@@ -10,37 +10,6 @@ const putPaint = async (
   image
 ) => {
   try {
-    const modelLower = model.toLowerCase();
-    const colorLower = color.toLowerCase();
-    const amountLower = amount.toLowerCase();
-    const existingPaint = await Paint.findOne({ _id: id });
-
-    if (!existingPaint || existingPaint.isActive === false) {
-      throw new Error('La pintura no existe');
-    } else {
-      if (price) {
-        existingPaint.price = price;
-      }
-      if (model) {
-        existingPaint.model = modelLower;
-      }
-      if (color) {
-        existingPaint.color = colorLower;
-      }
-      if (amount) {
-        existingPaint.amount = amountLower;
-      }
-      if (description) {
-        existingPaint.description = description;
-      }
-      if (image) {
-        existingPaint.image = image;
-      }
-      await existingPaint.save();
-      return {
-        message: 'La pintura fue actualizado'
-      };
-    }
   } catch (error) {
     return error.message;
   }
