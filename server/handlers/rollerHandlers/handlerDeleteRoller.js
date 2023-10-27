d77c2c91ac94ebdfdc12636e12f40756b6d176fd
franjunoy <franjunoy@hotmail.com>
2023-10-27 18:11:53 -0300

    Handler

--- server/handlers/rollerHandlers/handlerDeleteRoller.js
+++ server/handlers/rollerHandlers/handlerDeleteRoller.js
@@ -2,17 +2,6 @@ import { Roller } from '../../models/roller';
 
 const deleteRoller = async (id) => {
   try {
-    const existingRoller = await Roller.findOne({ _id: id });
-
-    if (!existingRoller || existingRoller.isActive === false) {
-      throw new Error('No se encontró el rodillo');
-    } else {
-      existingRoller.isActive = false;
-      await existingRoller.save();
-      return {
-        message: 'Eliminaste el rodillo'
-      };
-    }
   } catch (error) {
     return error.message;
   }
