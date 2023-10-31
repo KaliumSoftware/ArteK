a6873ef2381d479172121ab8d02cbc7a746d8215
franjunoy <franjunoy@hotmail.com>
2023-10-31 19:58:54 -0300

    handlerPutPaintbrush

--- server/handlers/paintbrushHandlers/handlerPutPaintbrush.js
+++ server/handlers/paintbrushHandlers/handlerPutPaintbrush.js
@@ -1,17 +1,38 @@
 import { putPaintbrush } from '../../controllers/paintbrushControllers/putPaintbrush';
 
-const handlerPutPaintbrush = async (
-  id,
-  price,
-  model,
-  color,
-  size,
-  description,
-  image
-) => {
+const handlerPutPaintbrush = async (req, res) => {
   try {
+    const { price, model, color, size, description } = req.body;
+    const { id } = req.params;
+    let image;
+
+    if (!price && !model && !color && !size && !description) {
+      throw new Error('Faltan datos');
+    } /* else if (req.files?.image) {
+      const result = await uploadServiceImage(req.files.image.tempFilePath);
+      image = {
+        public_id: result.public_id,
+        secure_url: result.secure_url
+      };
+      await fs.unlink(req.files.image.tempFilePath);
+    } */ else {
+      const updatedPaintbrush = await putPaintbrush(
+        id,
+        price,
+        model,
+        color,
+        size,
+        description
+      );
+
+      if (updatedPaintbrush.message === 'El cepillo no existe') {
+        throw new Error('No existe ese cepillo');
+      } else {
+        res.status(200).json(updatedPaintbrush);
+      }
+    }
   } catch (error) {
-    return error.message;
+    res.status(400).json({ error: error.message });
   }
 };
 
