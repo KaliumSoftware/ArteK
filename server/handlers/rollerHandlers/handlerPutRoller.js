c9a318ffc0f1a3b6aa9f5db1d1d2efd0c3e09bfb
franjunoy <franjunoy@hotmail.com>
2023-11-03 21:05:47 -0300

    handlerPutRoller

--- server/handlers/rollerHandlers/handlerPutRoller.js
+++ server/handlers/rollerHandlers/handlerPutRoller.js
@@ -10,8 +10,38 @@ const handlerPutRoller = async (
   image
 ) => {
   try {
+    const { price, model, color, amount, description } = req.body;
+    const { id } = req.params;
+    let image;
+
+    if (!price && !model && !color && !amount && !description && !image) {
+      throw new Error('Faltan datos');
+    } /* else if (req.files?.image) {
+      const result = await uploadServiceImage(req.files.image.tempFilePath);
+      image = {
+        public_id: result.public_id,
+        secure_url: result.secure_url
+      };
+      await fs.unlink(req.files.image.tempFilePath);
+    } */ else {
+      const updatedPaint = await putPaint(
+        id,
+        price,
+        model,
+        color,
+        amount,
+        description,
+        image
+      );
+
+      if (updatedPaint.message === 'La pintura no existe') {
+        throw new Error('No existe esa pintura');
+      } else {
+        res.status(200).json(updatedPaint);
+      }
+    }
   } catch (error) {
-    return error.message;
+    res.status(400).json({ error: error.message });
   }
 };
 
