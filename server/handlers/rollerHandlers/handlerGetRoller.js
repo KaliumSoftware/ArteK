a2fe92436cfabe2049a84c7be92617945603d0f5
franjunoy <franjunoy@hotmail.com>
2023-11-03 21:02:05 -0300

    handlerGetRoller

--- server/handlers/rollerHandlers/handlerGetRoller.js
+++ server/handlers/rollerHandlers/handlerGetRoller.js
@@ -2,8 +2,13 @@ import { getRoller } from '../../controllers/rollerControllers/getRoller';
 
 const handlerGetRoller = async () => {
   try {
+    const allPaint = await getPaint();
+    if (allPaint.message === 'No hay pinturas') {
+      throw new Error('No se encontraron pinturas');
+    }
+    res.status(200).json(allPaint);
   } catch (error) {
-    return error.message;
+    res.status(400).json({ error: error.message });
   }
 };
 
