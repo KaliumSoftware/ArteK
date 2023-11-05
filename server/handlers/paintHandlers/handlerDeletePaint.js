f1390df08f4c116ff588a11094b5fe396cb51ccc
franjunoy <franjunoy@hotmail.com>
2023-11-05 19:26:08 -0300

    Errores ortograficos

--- server/handlers/paintHandlers/handlerDeletePaint.js
+++ server/handlers/paintHandlers/handlerDeletePaint.js
@@ -6,7 +6,7 @@ const handlerDeletePaint = async (req, res) => {
 
     const deletedPaint = await deletePaint(id);
     if (deletedPaint.message === 'No se encontró la pintura') {
-      throw new Error('No existe una pintura con este nombre');
+      throw new Error('No existe esa pintura');
     } else {
       res.status(200).json({
         message: 'Pintura eliminado correctamente'
