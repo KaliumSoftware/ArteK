7743de426d278988388ec73d1c3f58e3b9680cec
franjunoy <franjunoy@hotmail.com>
2023-11-08 18:20:41 -0300

    handlerDeleteUser

--- server/handlers/userHandlers/handlerDeleteUser.js
+++ server/handlers/userHandlers/handlerDeleteUser.js
@@ -1,7 +1,17 @@
 import { deleteUser } from '../../controllers/userControllers/deleteUser';
 
 const handlerDeleteUser = async (req, res) => {
+  const { id } = req.params;
+
   try {
+    const deletedUser = await deleteUser(id);
+    if (deletedUser.message === 'No se encontró usuarioo') {
+      throw new Error('No existe es usuario');
+    } else {
+      res.status(200).json({
+        message: 'Usuario eliminado correctamente'
+      });
+    }
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
