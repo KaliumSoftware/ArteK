7516081fe35f321bca0020aebed0a15cc1706f33
franjunoy <franjunoy@hotmail.com>
2023-11-10 20:16:11 -0300

    handlerPutUser

--- server/handlers/userHandlers/handlerPutUser.js
+++ server/handlers/userHandlers/handlerPutUser.js
@@ -2,6 +2,15 @@ import { putUser } from '../../controllers/userControllers/putUser';
 
 const handlerPutUser = async (req, res) => {
   try {
+    const { id } = req.params;
+
+    const updatedUser = await putUser(id);
+
+    if (updatedUser.message === 'El usuario ya existe') {
+      throw new Error('El usuario ingresado ya existe');
+    } else {
+      res.status(200).json(updatedUser);
+    }
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
